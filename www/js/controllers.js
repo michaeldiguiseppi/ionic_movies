e9572b47018e620fa6e1c9306019b28b89f8569a
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-17 11:10:54 -0600

    Added barcode scanner.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -15,57 +15,97 @@ angular.module('starter.controllers', [])
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
+    scope: $scope,
+  }).then(function (modal) {
     $scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeLogin = function () {
     $scope.modal.hide();
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.login = function () {
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
+  $scope.doLogin = function () {
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
+    $timeout(function () {
       $scope.closeLogin();
     }, 1000);
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
+.controller('PlaylistsCtrl', function ($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
     { title: 'Dubstep', id: 3 },
     { title: 'Indie', id: 4 },
     { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Cowbell', id: 6, },
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PlaylistCtrl', function ($scope, $stateParams) {
 })
 
-.controller('BrowseCtrl', function($scope, $cordovaBarcodeScanner) {
-  $scope.scan = $cordovaBarcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
+.controller('BrowseCtrl', function ($scope, $cordovaBarcodeScanner, $http) {
+  $scope.scan = function () {
+    $cordovaBarcodeScanner.scan()
+    .then(function (result) {
+          $http({
+            method: 'GET',
+            url: 'http://localhost:5001/api/' + result.text,
+          })
+          .then(function (data) {
+            $http({
+              method: 'GET',
+              url: 'http://www.omdbapi.com/?t=' + data.DVD_Title,
+            })
+            .then(function (data) {
+              $scope.data = data;
+              console.log(data);
+              $ionicModal.fromTemplateUrl('../templates/result_modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+              }).then(function(modal) {
+                $scope.modal = modal;
+              });
+              $scope.openModal = function() {
+                $scope.modal.show();
+              };
+              $scope.closeModal = function() {
+                $scope.modal.hide();
+              };
+              // Cleanup the modal when we're done with it!
+              $scope.$on('$destroy', function() {
+                $scope.modal.remove();
+              });
+              // Execute action on hide modal
+              $scope.$on('modal.hidden', function() {
+                // Execute action
+              });
+              // Execute action on remove modal
+              $scope.$on('modal.removed', function() {
+                // Execute action
+              });
+            });
+          });
+
+          // alert('We got a barcode\n' +
+          //     'Result: ' + result.text + '\n' +
+          //     'Format: ' + result.format + '\n' +
+          //     'Cancelled: ' + result.cancelled);
+        })
+    .catch(function (error) {
+        alert('Scanning failed: ' + error);
+      });
+  };
 });
