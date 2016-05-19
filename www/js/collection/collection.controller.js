54fbec6a359dc1c3bdb41249fad15597925aaed1
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-19 11:38:06 -0600

    Changed size of search bar, and removed  from controllers.

--- www/js/collection/collection.controller.js
+++ www/js/collection/collection.controller.js
@@ -1,11 +1,9 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('CollectionCtrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('CollectionCtrl', ['$scope', 'httpService', function ($scope, httpService) {
       $scope.getMovies = function() {
-        $http({
-          method: 'GET',
-          url: 'http://10.2.12.11:3000/api/movies',
-        }).then(function(data) {
+        httpService.getAllMovies()
+        .then(function(data) {
           $scope.movies = data.data;
         }).catch(function(err) {
           $scope.message = {status: 'danger', data: err};
