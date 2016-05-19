8c696cbea434b85ef8347a51cac0bed572bc88c2
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-19 15:02:13 -0600

    Added new formatting.

--- www/js/movie/movie.controller.js
+++ www/js/movie/movie.controller.js
@@ -1,10 +1,14 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('MovieCtrl', ['$scope', 'httpService', '$stateParams', function ($scope, httpService, $stateParams) {
+    .controller('MovieCtrl', ['$scope', 'httpService', '$stateParams', '$ionicLoading',
+     function ($scope, httpService, $stateParams, $ionicLoading) {
+      $ionicLoading.show();
       httpService.getOneMovie($stateParams.title)
       .then(function(data) {
+        $ionicLoading.hide();
         $scope.movie = data.data;
       }).catch(function(err) {
+        $ionicLoading.hide();
         $scope.message = {status: 'danger', data: err};
       });
     }]);
