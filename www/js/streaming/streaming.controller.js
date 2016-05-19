e5b8d13354b4273dee1aaa6f18e00b11ae5c3047
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-19 15:02:27 -0600

    Added formatting for streaming sources.

--- www/js/streaming/streaming.controller.js
+++ www/js/streaming/streaming.controller.js
@@ -1,6 +1,13 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('StreamCtrl', ['$scope', 'httpService', function($scope, httpService) {
-      
+    .controller('StreamCtrl', ['$scope', 'httpService', '$stateParams',function($scope, httpService, $stateParams) {
+      httpService.getStreamingSources($stateParams.id)
+        .then(function(data) {
+          console.log(data);
+          $scope.movie = data.data;
+        })
+        .catch(function(err) {
+          $scope.message = {status: 'danger', data: err};
+        });
     }]);
 })();
