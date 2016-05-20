523104b5cde0fbb2b7bd356dee8b02eda093c66a
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-19 23:09:58 -0600

    Added styling for streaming sources and added a streaming_series template for TV shows.

--- www/js/streaming/streaming.controller.js
+++ www/js/streaming/streaming.controller.js
@@ -1,13 +1,22 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('StreamCtrl', ['$scope', 'httpService', '$stateParams',function($scope, httpService, $stateParams) {
-      httpService.getStreamingSources($stateParams.id)
+    .controller('StreamCtrl', ['$scope', 'httpService', '$stateParams', '$ionicLoading',
+    function($scope, httpService, $stateParams, $ionicLoading) {
+      $ionicLoading.show();
+      httpService.getStreamingSources($stateParams.id, $stateParams.type)
         .then(function(data) {
+          $ionicLoading.hide();
           console.log(data);
           $scope.movie = data.data;
         })
         .catch(function(err) {
+          $ionicLoading.hide();
           $scope.message = {status: 'danger', data: err};
         });
+
+        $scope.GoToLink = function(url) {
+          window.open(url,'_system');
+        };
+
     }]);
 })();
