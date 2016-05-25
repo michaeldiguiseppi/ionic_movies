9b351f15a959c44acef7926307d6b0fd044f09f6
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-25 15:37:17 -0600

    Fixed notifications on collection page.

--- www/js/collection/collection.controller.js
+++ www/js/collection/collection.controller.js
@@ -1,22 +1,26 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('CollectionCtrl', ['$scope', 'httpService', function ($scope, httpService) {
-      $scope.getMovies = function() {
-        httpService.getAllMovies()
-        .then(function(data) {
-          $scope.movies = data.data;
-        }).catch(function(err) {
-          $scope.message = {status: 'danger', data: err};
-        });
-      };
+    .controller('CollectionCtrl', ['$scope', 'httpService', '$ionicLoading',
+      function ($scope, httpService, $ionicLoading) {
+        $scope.getMovies = function() {
+          $ionicLoading.show();
+          httpService.getAllMovies()
+          .then(function(data) {
+            $ionicLoading.hide();
+            $scope.movies = data.data;
+          }).catch(function(err) {
+            $ionicLoading.hide();
+            $scope.message = {status: 'danger', data: 'There was an error getting your collection.  Please try again.'};
+          });
+        };
 
-      $scope.delete = function(id) {
-        httpService.deleteMovie(id)
-        .then(function(data) {
-          $scope.getMovies();
-        }).catch(function(err) {
-          $scope.message = {status: 'danger', data: err};
-        });
-      };
+        $scope.delete = function(id) {
+          httpService.deleteMovie(id)
+          .then(function(data) {
+            $scope.getMovies();
+          }).catch(function(err) {
+            $scope.message = {status: 'danger', data: 'There was an error deleting the movie.  Please try again.'};
+          });
+        };
     }]);
 })();
