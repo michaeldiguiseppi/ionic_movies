12e0484b14f912080fe8c145e26b8ae0d6309fc7
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-25 15:36:54 -0600

    Got register functionality working.

--- www/js/auth/register.controller.js
+++ www/js/auth/register.controller.js
@@ -4,19 +4,28 @@
 
     // Form data for the login modal
     $scope.registerData = {};
+    $scope.message = {};
 
     // Perform the login action when the user submits the login form
     $scope.doRegister = function() {
       $ionicLoading.show();
-      authService.register($scope.registerData).then(function(data) {
-        console.log(data);
+      authService.register($scope.registerData)
+      .then(function(data) {
         $ionicLoading.hide();
         authService.setUserInfo(data);
         $ionicHistory.nextViewOptions(
           { historyRoot: true }
         );
         $scope.registerData = {};
+        $scope.message = {};
         $state.go('app.search', {reload: true});
+      })
+      .catch(function(err) {
+        $ionicLoading.hide();
+        $scope.message = {
+          status: 'danger',
+          data: 'Email and/or password are incorrect.  Please try again.'
+        };
       });
     };
   });
