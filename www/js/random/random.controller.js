ef62380abe43ef7bd57b5aa9f25afc930fe7fe2e
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-28 13:14:57 -0600

    Added random movie feature.

--- www/js/random/random.controller.js
+++ www/js/random/random.controller.js
@@ -1,6 +1,14 @@
 (function() {
   angular.module('starter.controllers')
-    .controller('RandomCtrl', ['$scope', function($scope) {
-
+    .controller('RandomCtrl', ['$scope', 'httpService', function($scope, httpService) {
+      $scope.$on('$ionicView.enter', function(e) {
+        $scope.movie = {};
+        httpService.getAllMovies().then(function(data) {
+          var random = Math.floor(Math.random() * data.data.length);
+          console.log(data);
+          console.log(random);
+          $scope.movie = data.data[random];
+        });
+      });
     }]);
 })();
